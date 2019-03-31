6ae47e74d08b472d7d40280545122d8a39f8ef95
darachm <dchmiller@gmail.com>
2019-03-31 09:32:17 -0700

    Updated with a function and a makefile, so now it makes a yaml

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+
+gnostic.yaml: dekorne_gnostic_pages
+	python3 gnostic_parser.py
--- gnostic_parser.py
+++ gnostic_parser.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
-import re, yaml, bs4, os
+import re, ruamel.yaml, bs4, os
+
+hexes = dict()
+
+def parseOutNotes( list_of_ps ):
+    return_string = ""
+    # scan for it
+    while list_of_ps:
+        i = list_of_ps.pop(0)
+        if re.search("NOTES AND PARAPHRASES",str(i)):
+            break
+    # record it
+    while list_of_ps:
+        i = list_of_ps.pop(0)
+        if re.search("Line",str(i)): 
+            list_of_ps.insert(0,i)
+            break
+        return_string += str(i)
+    return( ( return_string, list_of_ps ) )
 
 for each in os.scandir("dekorne_gnostic_pages"):
     if each.is_file():
@@ -9,22 +27,19 @@ for each in os.scandir("dekorne_gnostic_pages"):
             soup = bs4.BeautifulSoup(html, 'html.parser')
 
             hex_number = soup.find(id=re.compile("hex"))['id']
-            print(hex_number)
+            hexes[hex_number] = dict()
+            hexes[hex_number]["notes"] = ""
+            hexes[hex_number]["lines"] = dict()
 
             plist = soup.find(id=re.compile("hex")).find_all("p")
 
-            while plist:
-                i = plist.pop(0)
-                if re.search("NOTES AND PARAPHRASES",str(i)):
-                    break
+            ( hexes[hex_number]["notes"], plist ) = parseOutNotes(plist)
+            ( hexes[hex_number]["lines"][0], plist ) = parseOutNotes(plist)
+            ( hexes[hex_number]["lines"][1], plist ) = parseOutNotes(plist)
+            ( hexes[hex_number]["lines"][2], plist ) = parseOutNotes(plist)
+            ( hexes[hex_number]["lines"][3], plist ) = parseOutNotes(plist)
+            ( hexes[hex_number]["lines"][4], plist ) = parseOutNotes(plist)
+            ( hexes[hex_number]["lines"][5], plist ) = parseOutNotes(plist)
 
-            while plist:
-                i = plist.pop(0)
-                if re.search("Line-1",str(i)):
-                    break
-                print(i)
-
-            print()
-            print()
-            print()
-            print()
+with open("gnostic.yaml","w") as f:
+    ruamel.yaml.dump(hexes,f)
